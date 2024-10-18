498b35818fa5f20a84c91767a8eb44a88a3c3ad1
Dion Cunningham <dion.cunningham86@hotmail.com>
2024-10-18 11:09:30 +1000

    Updated comments
    Added comments to files

--- Color Scheme Picker/index.js
+++ Color Scheme Picker/index.js
@@ -1,52 +1,57 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
-let schemeArray = []
+let schemeArray = [];
 
-// Load Color Scheme 
+// Load Color Scheme
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://www.thecolorapi.com/scheme?hex=F55A5A&mode=monochrome&count=5'
-    
-    fetch(url, { method: 'GET' })
-        .then(res => res.json())
-        .then(scheme => {
-            scheme.colors.forEach(color => {                
-                schemeArray.push(color.hex.value)
-            })
-            renderColors(schemeArray)                           
-        })
-})
+  const url =
+    'https://www.thecolorapi.com/scheme?hex=F55A5A&mode=monochrome&count=5';
+
+  fetch(url, { method: 'GET' })
+    .then((res) => res.json())
+    .then((scheme) => {
+      scheme.colors.forEach((color) => {
+        schemeArray.push(color.hex.value);
+      });
+      renderColors(schemeArray);
+    });
+});
 
 // Selected Color Scheme
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-    const colorInput = document.getElementById('color-input')
-    const colorScheme = document.getElementById('color-scheme')
-    
-    const url = `https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${colorScheme.value}&count=5`
-    let schemeArray = []
-    
-    fetch(url, { method: 'GET' })
-        .then(res => res.json())
-        .then(scheme => {
-            scheme.colors.forEach(color => {
-                schemeArray.push(color.hex.value)
-            })
-            renderColors(schemeArray)            
-        })    
-})
+  e.preventDefault();
+
+  const colorInput = document.getElementById('color-input');
+  const colorScheme = document.getElementById('color-scheme');
+
+  // Insert both input values to the url
+  const url = `https://www.thecolorapi.com/scheme?hex=${colorInput.value.slice(1)}&mode=${colorScheme.value}&count=5`;
+
+  // Ensure you empty the array for 5 "new" incoming colors
+  let schemeArray = [];
+
+  fetch(url, { method: 'GET' })
+    .then((res) => res.json())
+    .then((scheme) => {
+      scheme.colors.forEach((color) => {
+        schemeArray.push(color.hex.value);
+      });
+      renderColors(schemeArray);
+    });
+});
 
 // Render Color Scheme
 function renderColors(array) {
-    let colorHtml = ``
-    
-    array.forEach(color => {
-        colorHtml += `
+  let colorHtml = ``;
+
+  array.forEach((color) => {
+    colorHtml += `
             <div class="scheme-container">
                 <div class="color" style="background-color: ${color}"></div>
                 <p class="hex-code">${color}</p>
             </div>
-        `
-    })
-    document.getElementById('scheme-grid').innerHTML = colorHtml      
+        `;
+  });
+  // Load all elements to the page
+  document.getElementById('scheme-grid').innerHTML = colorHtml;
 }
